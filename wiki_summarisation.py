3e35aceb8daef92629abb0d2e9cf32d8f2a96ba9
olyatyntareva <157363979+olyatyntareva@users.noreply.github.com>
2024-03-11 10:10:42 +0300

    wiki_summarisation_2.py

--- wiki_summarisation.py
+++ wiki_summarisation.py
@@ -27,50 +27,50 @@ while True:
         article_link = article.url
         print(f"Название статьи: {article.title} {article_link}")
 
-        # wikipedia.exceptions.PageError: (статья не существует, например, не переведена на язык запроса -- русский)
+        webbrowser.open(article_link)
+
+        # Парсер статьи
+        parser = PlaintextParser.from_string(article.content, Tokenizer("russian"))
+        tokenized_text = word_tokenize(article.content.lower())
+
+        # LexRank -- Неконтролируемый подход к обобщению текста, основанный на оценке центральности предложений на основе графов.
+        # Основная идея заключается в том, что предложения «рекомендуют» читателю другие подобные предложения.
+        # Таким образом, если одно предложение очень похоже на многие другие, оно, скорее всего, будет предложением большой важности.
+        Summarizer_Lex = LexRankSummarizer()
+        Summary_1 = Summarizer_Lex(parser.document, 10)
+        print("\nLexRank суммаризация:")
+        print(' '.join([str(sentence) for sentence in Summary_1]))
+
+        # Luhn  -- на основе частоты наиболее важных слов
+        Summarizer_Luhn = LuhnSummarizer()
+        Summary_2 = Summarizer_Luhn(parser.document, 10)
+        print("\nLuhn суммаризация:")
+        print(' '.join([str(sentence) for sentence in Summary_2]))
+
+        # SumBasic
+        SummarizerSB = SumBasicSummarizer()
+        Summary_3 = SummarizerSB(parser.document, 10)
+        print("\nSumBasic суммаризация:")
+        print(' '.join([str(sentence) for sentence in Summary_3]))
+
+        # Автоматическое создание списков с эвристиками для Edmundson
+        FDist = FreqDist(tokenized_text)
+        common_words = FDist.most_common(10)
+
+        Bonus_words = [word for word, _ in common_words]
+        Stigma_words = [word for word, _ in reversed(common_words)]
+        Null_words = [word for word, _ in common_words]
+
+        # Edmudson
+        Summarizer_Ed = EdmundsonSummarizer()
+        Summarizer_Ed.bonus_words = Bonus_words
+        Summarizer_Ed.stigma_words = Stigma_words
+        Summarizer_Ed.null_words = Null_words
+        Summary_4 = Summarizer_Ed(parser.document, 10)
+        print("\nEdmudson суммаризация:")
+        print(' '.join([str(sentence) for sentence in Summary_4]))
+        break
+
+    # wikipedia.exceptions.PageError: (статья не существует, например, не переведена на язык запроса -- русский)
     except (wikipedia.exceptions.PageError):
         print(f"Статья не существует. Повторная попытка...")
-
-    webbrowser.open(article_link)
-
-    # Парсер статьи
-    parser = PlaintextParser.from_string(article.content, Tokenizer("russian"))
-    tokenized_text = word_tokenize(article.content.lower())
-
-    # LexRank -- Неконтролируемый подход к обобщению текста, основанный на оценке центральности предложений на основе графов.
-    # Основная идея заключается в том, что предложения «рекомендуют» читателю другие подобные предложения.
-    # Таким образом, если одно предложение очень похоже на многие другие, оно, скорее всего, будет предложением большой важности.
-    Summarizer_Lex = LexRankSummarizer()
-    Summary_1 = Summarizer_Lex(parser.document, 10)
-    print("\nLexRank суммаризация:")
-    print(' '.join([str(sentence) for sentence in Summary_1]))
-
-    # Luhn  -- на основе частоты наиболее важных слов
-    Summarizer_Luhn = LuhnSummarizer()
-    Summary_2 = Summarizer_Luhn(parser.document, 10)
-    print("\nLuhn суммаризация:")
-    print(' '.join([str(sentence) for sentence in Summary_2]))
-
-    # SumBasic
-    SummarizerSB = SumBasicSummarizer()
-    Summary_3 = SummarizerSB(parser.document, 10)
-    print("\nSumBasic суммаризация:")
-    print(' '.join([str(sentence) for sentence in Summary_3]))
-
-    # Автоматическое создание списков с эвристиками для Edmundson
-    FDist = FreqDist(tokenized_text)
-    common_words = FDist.most_common(10)
-
-    Bonus_words = [word for word, _ in common_words]
-    Stigma_words = [word for word, _ in reversed(common_words)]
-    Null_words = [word for word, _ in common_words]
-
-    # Edmudson
-    Summarizer_Ed = EdmundsonSummarizer()
-    Summarizer_Ed.bonus_words = Bonus_words
-    Summarizer_Ed.stigma_words = Stigma_words
-    Summarizer_Ed.null_words = Null_words
-    Summary_4 = Summarizer_Ed(parser.document, 10)
-    print("\nEdmudson суммаризация:")
-    print(' '.join([str(sentence) for sentence in Summary_4]))
-    break
